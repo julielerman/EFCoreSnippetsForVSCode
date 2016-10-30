ef9123a1413da307897bbc30804f540d34d0d876
julie <julielerman@gmail.com>
2016-10-29 19:56:40 -0700

    first snippets

--- Snippets.cs
+++
@@ -1,7 +0,0 @@
-   "DbContext OnConfiguring": {
-        "prefix": "override onconfiguring",
-        "body": [
-            "protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {",
-            "}"
-        ]
-    }
